59b58b9a373c334f07912310db4bad26d4e4283e
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2019-02-20 15:12:00 +0000

    PP-4894: Update existing stripe spec to use runner

--- app/web/modules/stripe/stripe.model.spec.js
+++ app/web/modules/stripe/stripe.model.spec.js
@@ -1,77 +1,88 @@
 const StripeAccount = require('./stripe.model')
 
-// @TODO(mrlumbu) Make these proper
-const stripeAccount1 = new StripeAccount({
-  org_id: 'org_id',
-  org_name: 'org_name',
-  org_ip_address: 'org_ip_address',
-  org_statement_descriptor: 'org_statement_descriptor',
-  org_phone_number: 'org_phone_number'
+const { expect } = require('chai')
+
+describe('Stripe Account model', () => {
+  it('validates model with core details', () => {
+    expect(() => {
+      const stripeAccount1 = new StripeAccount({
+        org_id: 'org_id',
+        org_name: 'org_name',
+        org_ip_address: 'org_ip_address',
+        org_statement_descriptor: 'org_statement_descriptor',
+        org_phone_number: 'org_phone_number'
+      })
+    }).to.not.throw()
+  })
+
+  it('ensures no bank account entity with partially provided details', () => {
+    const stripeAccount2 = new StripeAccount({
+      org_id: 'org_id',
+      org_name: 'org_name',
+      org_ip_address: 'org_ip_address',
+      org_statement_descriptor: 'org_statement_descriptor',
+      org_phone_number: 'org_phone_number',
+      bank_account_number: '12345678'
+    })
+    expect(stripeAccount2).to.not.have.property('external_account')
+  })
+
+  it('includes bank account with enough details provided', () => {
+    const stripeAccount3 = new StripeAccount({
+      org_id: 'org_id',
+      org_name: 'org_name',
+      org_ip_address: 'org_ip_address',
+      org_statement_descriptor: 'org_statement_descriptor',
+      org_phone_number: 'org_phone_number',
+      bank_account_number: '12345678',
+      bank_account_sort_code: '123456'
+    })
+    expect(stripeAccount3).to.have.property('external_account')
+  })
+
+  it('includes partial fields for legal entity responsible person', () => {
+    // expect(() => {
+    const stripeAccount4 = new StripeAccount({
+      org_id: 'org_id',
+      org_name: 'org_name',
+      org_ip_address: 'org_ip_address',
+      org_statement_descriptor: 'org_statement_descriptor',
+      org_phone_number: 'org_phone_number',
+      bank_account_number: '12345678',
+      bank_account_sort_code: '123456',
+      person_dob_year: 2018,
+      person_dob_month: 1,
+      person_dob_day: 1
+    })
+    // }).to.throw('an error')
+    expect(stripeAccount4).to.deep.nested.include({
+      'legal_entity.dob.day': 1,
+      'legal_entity.dob.month': 1,
+      'legal_entity.dob.year': 2018
+    })
+  })
+
+  it('includes full legal entity with all provided fields', () => {
+    const stripeAccount5 = new StripeAccount({
+      org_id: 'org_id',
+      org_name: 'org_name',
+      org_ip_address: 'org_ip_address',
+      org_statement_descriptor: 'org_statement_descriptor',
+      org_phone_number: 'org_phone_number',
+      bank_account_number: '12345678',
+      bank_account_sort_code: '123456',
+      person_dob_year: 2018,
+      person_dob_month: 1,
+      person_dob_day: 1,
+      person_address_line_1: 'line 1',
+      person_address_city: 'city',
+      person_address_postcode: 'postcode',
+      person_first_name: 'joe',
+      person_last_name: 'bloggs'
+    })
+
+    expect(stripeAccount5).to.have.property('legal_entity')
+    expect(stripeAccount5).to.have.property('external_account')
+    expect(stripeAccount5.legal_entity).to.have.property('personal_address')
+  })
 })
-
-console.log(stripeAccount1)
-
-const stripeAccount2 = new StripeAccount({
-  org_id: 'org_id',
-  org_name: 'org_name',
-  org_ip_address: 'org_ip_address',
-  org_statement_descriptor: 'org_statement_descriptor',
-  org_phone_number: 'org_phone_number',
-  bank_account_number: '12345678'
-})
-
-//  expect no bank acount still
-console.log(stripeAccount2)
-
-const stripeAccount3 = new StripeAccount({
-  org_id: 'org_id',
-  org_name: 'org_name',
-  org_ip_address: 'org_ip_address',
-  org_statement_descriptor: 'org_statement_descriptor',
-  org_phone_number: 'org_phone_number',
-  bank_account_number: '12345678',
-  bank_account_sort_code: '123456'
-})
-
-//  expect bank acount added
-console.log(stripeAccount3)
-
-
-const stripeAccount4 = new StripeAccount({
-  org_id: 'org_id',
-  org_name: 'org_name',
-  org_ip_address: 'org_ip_address',
-  org_statement_descriptor: 'org_statement_descriptor',
-  org_phone_number: 'org_phone_number',
-  bank_account_number: '12345678',
-  bank_account_sort_code: '123456',
-  person_dob_year: 2018,
-  person_dob_month: 1,
-  person_dob_day: 1
-})
-
-//  expect bank acount added
-console.log(stripeAccount4)
-
-
-
-const stripeAccount5 = new StripeAccount({
-  org_id: 'org_id',
-  org_name: 'org_name',
-  org_ip_address: 'org_ip_address',
-  org_statement_descriptor: 'org_statement_descriptor',
-  org_phone_number: 'org_phone_number',
-  bank_account_number: '12345678',
-  bank_account_sort_code: '123456',
-  person_dob_year: 2018,
-  person_dob_month: 1,
-  person_dob_day: 1,
-  person_address_line_1: 'line 1',
-  person_address_city: 'city',
-  person_address_postcode: 'postcode',
-  person_first_name: 'joe',
-  person_last_name: 'bloggs'
-})
-
-//  expect bank acount added
-console.log(stripeAccount5)
