c94f182aab05362d9553159fcad25ec714a496c8
Stephen Daly <stephencdaly@gmail.com>
2019-04-05 10:55:53 +0100

    PP-5027 Add tests for gateway account model validation

--- src/web/modules/gateway_accounts/gateway_accounts.spec.js
+++ src/web/modules/gateway_accounts/gateway_accounts.spec.js
@@ -1,6 +1,8 @@
 const { expect } = require('chai')
+const _ = require('lodash')
 
 const GatewayAccount = require('./gatewayAccount.model.js')
+const { ValidationError } = require('./../../../lib/errors')
 
 const validGatewayAccountDetails = {
   live: 'live',
@@ -12,22 +14,148 @@ const validGatewayAccountDetails = {
   credentials: 'valid-credentials'
 }
 
-const invalidLiveSandboxAccountDetails = Object.assign({}, validGatewayAccountDetails, {
-  provider: 'card-sandbox'
-})
-
-describe('Gateway Accounts', () => {
+describe.only('Gateway Accounts', () => {
   describe('Modeling a Gateway Account', () => {
     it('successfully creates a model with valid details', () => {
       const account = new GatewayAccount(validGatewayAccountDetails)
       expect(account).to.be.an('object')
     })
 
-    it('rejects a model with invalid details', () => {
+    it('rejects when live field is empty', () => {
       expect(() => {
-        const account = new GatewayAccount(invalidLiveSandboxAccountDetails)
-        expect(account).to.be.an('object')
-      }).to.throw('live accounts cannot use Sandbox providers')
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.live = ''
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('rejects when live field is not allowed value', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.live = 'not-valid'
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('rejects when payment method is empty', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.paymentMethod = ''
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('rejects when payment method is not allowed value', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.paymentMethod = 'not-allowed'
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('rejects when description is empty', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.description = ''
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('rejects when serviceName is empty', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.serviceName = ''
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('rejects when analyticsId is empty', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.analyticsId = ''
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('rejects when paymentMethod is card and non-card provider given', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.paymentMethod = 'card'
+        details.provider = 'gocardless'
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('rejects when paymentMethod is direct-debit and non-direct-debit provider given', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.paymentMethod = 'direct-debit'
+        details.provider = 'stripe'
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('rejects when account is live and provider is card sandbox', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.live = 'live'
+        details.paymentMethod = 'card'
+        details.provider = 'card-sandbox'
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('rejects when account is live and provider is direct-debit sandbox', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.live = 'live'
+        details.paymentMethod = 'direct-debit'
+        details.provider = 'sandbox'
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
+    })
+
+    it('successfully creates model when provider is not stripe and credentials is empty', () => {
+      const details = _.cloneDeep(validGatewayAccountDetails)
+      details.provider = 'worldpay'
+      details.credentials = ''
+
+      const account = new GatewayAccount(details)
+      expect(account).to.be.an('object')
+    })
+
+    it('rejects when provider is stripe and no credentials provided', () => {
+      expect(() => {
+        const details = _.cloneDeep(validGatewayAccountDetails)
+        details.provider = 'stripe'
+        details.credentials = ''
+
+        // eslint-disable-next-line no-new
+        new GatewayAccount(details)
+      }).to.throw(ValidationError)
     })
   })
 })
+
