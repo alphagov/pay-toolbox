6762b845204a571f5abb6e945695364522949660
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2019-07-11 10:47:52 +0100

    BAU: remove custom type definition for https-proxy-agent
    * https-proxy-agent supports Typescript as of
    https://github.com/TooTallNate/node-https-proxy-agent/releases/tag/2.2.2
    * remove our type definition as there cannot be conflicting versions

--- src/web/modules/stripe/basic/basic.http.ts
+++ src/web/modules/stripe/basic/basic.http.ts
@@ -1,5 +1,5 @@
 import * as Stripe from 'stripe'
-import * as HTTPSProxyAgent from 'https-proxy-agent'
+import HTTPSProxyAgent from 'https-proxy-agent'
 import { Request, Response } from 'express'
 
 import * as logger from '../../../../lib/logger'
