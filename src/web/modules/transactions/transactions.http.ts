89d6407972fcd8620e6018f1b4811c33a7f2d0d2
Steven Fountain <steven.fountain@digital.cabinet-office.gov.uk>
2019-07-29 16:58:05 +0100

    PP-5389: transaction search handler + template

--- src/web/modules/transactions/transactions.http.ts
+++ src/web/modules/transactions/transactions.http.ts
@@ -5,6 +5,21 @@ import { Transaction } from 'ledger'
 import { Ledger, Connector, AdminUsers } from '../../../lib/pay-request'
 import * as logger from '../../../lib/logger'
 
+export async function searchPage(req: Request, res: Response): Promise<void> {
+  res.render('transactions/search', { csrf: req.csrfToken() })
+}
+
+export async function search(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const { id } = req.body
+
+    // most basic search implementation - just forward to transactions route
+    res.redirect(`/transactions/${id}`)
+  } catch (error) {
+    next(error)
+  }
+}
+
 export async function show(req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
     const transaction = await Ledger.transaction(req.params.id) as Transaction
