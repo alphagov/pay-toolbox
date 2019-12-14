47c984ccf801242a1c8c9559ea8a682ddcfe1dd5
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2019-12-14 00:17:38 +0000

    BAU: dashboard initial layout

--- src/browser/main.tsx
+++ src/browser/main.tsx
@@ -3,38 +3,61 @@ import React from 'react'
 import { render } from 'react-dom'
 
 interface DashboardState {
-  count: number
 }
 
 class Dashboard extends React.Component<{}, DashboardState> {
-  interval: NodeJS.Timeout
   constructor(props: {}) {
     super(props)
-    this.state = {
-      count: 0
-    }
+  }
 
-    this.interval = setInterval(() => this.tick(), 10)
-  }
-  tick() {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
   render() {
     return (
       <div>
-        <div className="govuk-grid-row">
+        <div className="govuk-grid-row govuk-body govuk-!-margin-bottom-4">
           <div className="govuk-grid-column-one-half">
-            <p className="govuk-body">govuk-grid-column-one-half {this.state.count}</p>
+            <div className="dashboard-card">
+              <p className="govuk-body">govuk-grid-column-one-half dashboard-card</p>
+            </div>
           </div>
           <div className="govuk-grid-column-one-half">
-          <p className="govuk-body">govuk-grid-column-one-half {this.state.count}</p>
+            <div className="dashboard-card">
+              <span className="govuk-caption-xl">Payments</span>
+              <h1 className="govuk-heading-xl">32,450</h1>
+              <span className="govuk-caption-xl">Gross volume</span>
+              <h1 className="govuk-heading-xl">£506,000</h1>
+
+              <table className="stats-table">
+
+                <tbody className="govuk-table__body">
+                  <tr className="govuk-table__row">
+                    <th scope="row" className="stats-cell">
+                      <span className="govuk-caption-m">All payments</span>
+                    </th>
+                    <td className="stats-cell">43,450</td>
+                  </tr>
+                  <tr className="govuk-table__row">
+                    <th scope="row" className="stats-cell">
+                      <span className="govuk-caption-m">All payments gross volume</span>
+                    </th>
+                    <td className="stats-cell">£750,000</td>
+                  </tr>
+                  <tr className="govuk-table__row">
+                    <th scope="row" className="stats-cell">
+                      <span className="govuk-caption-m">Completion rate</span>
+                    </th>
+                    <td className="stats-cell">83%</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
         <div className="govuk-grid-row">
           <div className="govuk-grid-column-full">
-            <p className="govuk-body">govuk-grid-column-full {this.state.count}</p>
+            <div className="dashboard-card">
+              <span className="govuk-caption-xl">Friday 13th December 2019</span>
+              <p className="govuk-body">Daily graph of payment volume, compared with last week</p>
+            </div>
           </div>
         </div>
       </div>
