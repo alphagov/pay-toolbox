533390591045747ab10f98b5e759c846275ae55a
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2019-12-14 01:25:07 +0000

    PP-5991: dynamic panel sizing with ResizeObserver
    * add resize observer polyfill library
    * use ResizeObserver browser hooks to ensure events list tracks the size
    of the StatsPanel

--- src/browser/main.tsx
+++ src/browser/main.tsx
@@ -1,55 +1,108 @@
+/* global ResizeObserver */
 import React from 'react'
 // import { hydrate } from 'react-dom'
 import { render } from 'react-dom'
 
+import ResizeObserver from '@juggle/resize-observer'
+
+class EventCard extends React.Component {
+  render() {
+    return (
+      <div className="dashboard-card govuk-!-margin-bottom-2">
+        <p className="govuk-body">govuk-grid-column-one-half dashboard-card</p>
+      </div>
+    )
+  }
+}
+
+interface StatsPanelProps {
+  watch: CallableFunction
+}
+
+class StatsPanel extends React.Component<StatsPanelProps, {}> {
+  constructor(props: StatsPanelProps) {
+    super(props)
+    this.setPanelRef = this.setPanelRef.bind(this)
+  }
+  setPanelRef(element: Element) {
+    this.props.watch(element)
+  }
+  render() {
+    return (
+      <div ref={this.setPanelRef} className="dashboard-card">
+        <span className="govuk-caption-xl">Payments</span>
+        <h1 className="govuk-heading-xl">32,450</h1>
+        <span className="govuk-caption-xl">Gross volume</span>
+        <h1 className="govuk-heading-xl">£506,000</h1>
+
+        <table className="stats-table">
+
+          <tbody className="govuk-table__body">
+            <tr className="govuk-table__row">
+              <th scope="row" className="stats-cell">
+                <span className="govuk-caption-m">All payments</span>
+              </th>
+              <td className="stats-cell">43,450</td>
+            </tr>
+            <tr className="govuk-table__row">
+              <th scope="row" className="stats-cell">
+                <span className="govuk-caption-m">All payments gross volume</span>
+              </th>
+              <td className="stats-cell">£750,000</td>
+            </tr>
+            <tr className="govuk-table__row">
+              <th scope="row" className="stats-cell">
+                <span className="govuk-caption-m">Completion rate</span>
+              </th>
+              <td className="stats-cell">83%</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
 interface DashboardState {
+  statsHeight: number
 }
 
 class Dashboard extends React.Component<{}, DashboardState> {
   constructor(props: {}) {
     super(props)
+    this.state = {
+      statsHeight: 0
+    }
+    this.setWatchObserver = this.setWatchObserver.bind(this)
+  }
+
+  setWatchObserver(element: Element) {
+    const resizeObserver = new ResizeObserver((entries) => {
+      const entry = entries[0]
+      this.setState({
+        statsHeight: entry.contentRect.height + 30
+      })
+    })
+
+    resizeObserver.observe(element)
   }
 
   render() {
     return (
       <div>
         <div className="govuk-grid-row govuk-body govuk-!-margin-bottom-4">
-          <div className="govuk-grid-column-one-half">
-            <div className="dashboard-card">
-              <p className="govuk-body">govuk-grid-column-one-half dashboard-card</p>
-            </div>
+          <div style={{ maxHeight: this.state.statsHeight, overflow: 'scroll' }} className="govuk-grid-column-one-half">
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
+          <EventCard />
           </div>
           <div className="govuk-grid-column-one-half">
-            <div className="dashboard-card">
-              <span className="govuk-caption-xl">Payments</span>
-              <h1 className="govuk-heading-xl">32,450</h1>
-              <span className="govuk-caption-xl">Gross volume</span>
-              <h1 className="govuk-heading-xl">£506,000</h1>
-
-              <table className="stats-table">
-
-                <tbody className="govuk-table__body">
-                  <tr className="govuk-table__row">
-                    <th scope="row" className="stats-cell">
-                      <span className="govuk-caption-m">All payments</span>
-                    </th>
-                    <td className="stats-cell">43,450</td>
-                  </tr>
-                  <tr className="govuk-table__row">
-                    <th scope="row" className="stats-cell">
-                      <span className="govuk-caption-m">All payments gross volume</span>
-                    </th>
-                    <td className="stats-cell">£750,000</td>
-                  </tr>
-                  <tr className="govuk-table__row">
-                    <th scope="row" className="stats-cell">
-                      <span className="govuk-caption-m">Completion rate</span>
-                    </th>
-                    <td className="stats-cell">83%</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <StatsPanel watch={this.setWatchObserver} />
           </div>
         </div>
         <div className="govuk-grid-row">
