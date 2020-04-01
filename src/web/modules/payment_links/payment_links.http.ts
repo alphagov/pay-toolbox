9b42894ca5ba2800a089149e11a3367acf96d85e
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-04-01 19:11:06 +0100

    PP-6325: payment links export csv route
    Allow payment links usage page to export CSV, this applies to both
    individual gateway account views and the entire platform.
    
    CSV generation should respect live account only filters and sorting on
    payment date and total payments.

--- src/web/modules/payment_links/payment_links.http.ts
+++ src/web/modules/payment_links/payment_links.http.ts
@@ -1,6 +1,7 @@
 import _ from 'lodash'
 import { Request, Response, NextFunction } from 'express'
 import { Products, AdminUsers, Connector } from '../../../lib/pay-request'
+import { Parser } from 'json2csv'
 
 function indexPaymentLinksByType(paymentLink: any): any {
   const index = paymentLink.product._links.reduce((aggregate: any, linkDetails: any) => {
@@ -13,66 +14,175 @@ function indexPaymentLinksByType(paymentLink: any): any {
 
 export async function list(req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
-    let serviceRequest, paymentLinksRequest, liveAccountsRequest
     const { accountId } = req.params
     const sort = req.query.sort || 'last_payment_date'
     const live = req.query.live === undefined ? true : req.query.live && req.query.live !== "false"
 
     // we're only going to filter live gateway accounts if we're at the whole platform level
     const filterLiveAccounts = live && !accountId
+    const usageReportResults = await fetchUsageContext(sort, filterLiveAccounts, accountId)
 
-    if (accountId) {
-      serviceRequest = AdminUsers.gatewayAccountServices(accountId)
-        .then((service: any) => [ service ])
-    } else {
-      serviceRequest = AdminUsers.services()
-    }
-
-    if (filterLiveAccounts) {
-      liveAccountsRequest = Connector.accounts({ type: 'live' })
-        .then((response: any) => response.accounts)
-    } else {
-      liveAccountsRequest = Promise.resolve([])
-    }
-
-    paymentLinksRequest = Products.paymentLinksWithUsage(accountId)
-
-    const [serviceResponse, paymentLinksResponse, liveAccountsResponse] = await Promise.all([serviceRequest, paymentLinksRequest, liveAccountsRequest])
-
-    const serviceGatewayAccountIndex = serviceResponse
-      .reduce((aggregate: any, service: any) => {
-        service.gateway_account_ids.forEach((accountId: string) => {
-          aggregate[accountId] = service.service_name && service.service_name.en
-        })
-        return aggregate
-      }, {})
-
-    const liveAccounts = liveAccountsResponse.map((account: any) => account.gateway_account_id)
-
-    const paymentLinks = paymentLinksResponse
-      .map(indexPaymentLinksByType)
-      .filter((link: any) => !filterLiveAccounts || liveAccounts.includes(link.product.gateway_account_id))
-
-    const groupedLinks = _.groupBy(paymentLinks, 'product.gateway_account_id')
-    const orderedGroups = _.orderBy(
-      Object.keys(groupedLinks)
-        .map((key: any) => {
-          const group = groupedLinks[key]
-          return {
-            key,
-            links: _.orderBy(group, sort, 'desc'),
-            payment_count: _.sumBy(group, 'payment_count'),
-            last_payment_date: _.orderBy(group, 'last_payment_date', 'desc')[0].last_payment_date
-          }
-        }),
+    const context = {
+      filterLiveAccounts,
       sort,
-      'desc'
-    )
+      accountId,
+      ...usageReportResults
+    }
 
-    res.render('payment_links/overview', {
-      groupedPaymentLinks: orderedGroups, accountId, serviceGatewayAccountIndex, sort, filterLiveAccounts
-    })
+    res.render('payment_links/overview', context)
   } catch (error) {
     next(error)
+  }
+}
+
+export async function listCSV(req: Request, res: Response, next: NextFunction): Promise<void> {
+   try {
+    const { accountId } = req.params
+    const sort = req.query.sort || 'last_payment_date'
+    const live = req.query.live === undefined ? true : req.query.live && req.query.live !== "false"
+
+    // we're only going to filter live gateway accounts if we're at the whole platform level
+    const filterLiveAccounts = live && !accountId
+    const usageReportResults = await fetchUsageContext(sort, filterLiveAccounts, accountId)
+
+    const flatLinksList = usageReportResults.groupedPaymentLinks
+      .reduce((aggregate: any, groupedList: any) => {
+        aggregate = [ ...aggregate, ...groupedList.links ]
+        return aggregate
+      }, [])
+
+    const fields = [{
+      label: 'ID',
+      value: 'product.external_id'
+    }, {
+      label: 'Gateway account is live',
+      value: 'is_live_account'
+    }, {
+      label: 'Gateway account ID',
+      value: 'product.gateway_account_id'
+    }, {
+      label: 'Number of payments',
+      value: 'payment_count'
+    }, {
+      label: 'Date last used',
+      value: 'last_payment_date'
+    },{
+      label: 'Payment link name',
+      value: 'product.name'
+    }, {
+      label: 'Payment link description',
+      value: 'product.description'
+    },{
+      label: 'Payment link URL',
+      value: 'url'
+    },{
+      label: 'Service name',
+      value: 'service_name'
+    },{
+      label: 'Organisation name',
+      value: 'organisation_name'
+    },{
+      label: 'Sector',
+      value: 'sector'
+    },{
+      label: 'Is fixed price',
+      value: 'is_fixed_price'
+    },{
+      label: 'Fixed price',
+      value: 'product.price'
+    },{
+      label: 'Custom reference entered',
+      value: 'product.reference_enabled'
+    },{
+      label: 'Reference label',
+      value: 'product.reference_label'
+    },{
+      label: 'Language',
+      value: 'product.language'
+    },{
+      label: 'Has metadata',
+      value: 'has_metadata'
+    },{
+      label: 'Metadata',
+      value: 'product.metadata'
+    }]
+
+    const parser = new Parser({ fields })
+    const csv = parser.parse(flatLinksList)
+    const name = accountId || 'platform'
+    res.set('Content-Type', 'text/csv')
+    res.set('Content-Disposition', `attachment; filename="GOVUK_Pay_payment_links_usage_${ name }.csv"`)
+    res.status(200).send(csv)
+   } catch(error) {
+     next(error)
+   }
+}
+
+interface PaymentLinkUsageContext {
+  groupedPaymentLinks: any
+  serviceGatewayAccountIndex: any
+}
+async function fetchUsageContext(sortKey: string, filterLiveAccounts: Boolean, accountId?: string): Promise<PaymentLinkUsageContext> {
+  let serviceRequest, paymentLinksRequest, liveAccountsRequest
+  if (accountId) {
+    serviceRequest = AdminUsers.gatewayAccountServices(accountId)
+      .then((service: any) => [ service ])
+    liveAccountsRequest = Connector.account(accountId)
+    .then((account: any) => [ account ])
+  } else {
+    serviceRequest = AdminUsers.services()
+    liveAccountsRequest = Connector.accounts({ type: 'live' })
+      .then((response: any) => response.accounts)
+  }
+
+  paymentLinksRequest = Products.paymentLinksWithUsage(accountId)
+
+  const [serviceResponse, paymentLinksResponse, liveAccountsResponse] = await Promise.all([serviceRequest, paymentLinksRequest, liveAccountsRequest])
+
+  const serviceGatewayAccountIndex = serviceResponse
+    .reduce((aggregate: any, service: any) => {
+      service.gateway_account_ids.forEach((accountId: string) => {
+        aggregate[accountId] = service
+      })
+      return aggregate
+    }, {})
+
+  const liveAccounts = liveAccountsResponse.map((account: any) => account.gateway_account_id)
+
+  const paymentLinks = paymentLinksResponse
+    .map(indexPaymentLinksByType)
+    .filter((link: any) => !filterLiveAccounts || liveAccounts.includes(link.product.gateway_account_id))
+    .map((link: any) => {
+      const service = serviceGatewayAccountIndex[link.product.gateway_account_id]
+      return {
+        ...link,
+        is_live_account: liveAccounts.includes(link.product.gateway_account_id),
+        url: link.product._indexedLinks.friendly || link.product._indexedLinks.pay,
+        service_name: service && service.service_name && service.service_name.en,
+        organisation_name: service && service.merchant_details && service.merchant_details.name,
+        is_fixed_price: Boolean(link.product.price),
+        has_metadata: Boolean(link.product.metadata)
+      }
+    })
+
+  const groupedLinks = _.groupBy(paymentLinks, 'product.gateway_account_id')
+  const orderedGroups = _.orderBy(
+    Object.keys(groupedLinks)
+      .map((key: any) => {
+        const group = groupedLinks[key]
+        return {
+          key,
+          links: _.orderBy(group, sortKey, 'desc'),
+          payment_count: _.sumBy(group, 'payment_count'),
+          last_payment_date: _.orderBy(group, 'last_payment_date', 'desc')[0].last_payment_date,
+          is_live_account: liveAccounts.includes(key)
+        }
+      }),
+    sortKey,
+    'desc'
+  )
+  return {
+    groupedPaymentLinks: orderedGroups,
+    serviceGatewayAccountIndex
   }
 }
