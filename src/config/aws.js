bcf947dfca4a0b3072fe837b1dd14f036604b559
Stephen Daly <stephencdaly@gmail.com>
2020-04-17 11:43:08 +0100

    PP-6219 Upload file to S3
    Once the transactions update CSV has been validated, upload it to S3

---
+++ src/config/aws.js
@@ -0,0 +1,17 @@
+const Joi = require('joi')
+
+const expectedAwsEnvironmentValues = {
+  AWS_S3_UPDATE_TRANSACTIONS_BUCKET_NAME: Joi.string()
+}
+
+const { error, value: validatedAwsEnvironmentValues } = Joi.validate(
+  process.env,
+  expectedAwsEnvironmentValues,
+  { allowUnknown: true, stripUnknown: true }
+)
+
+if (error) {
+  throw new Error(`Invalid aws environment variables set ${error.message}`)
+}
+
+module.exports = validatedAwsEnvironmentValues
