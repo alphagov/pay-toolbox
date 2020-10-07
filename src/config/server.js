c6662942629d3627c85516eee3b84d850c5ad961
Steven <steven.fountain@digital.cabinet-office.gov.uk>
2020-10-07 10:57:41 +0100

    PP-7267: use `secureProxy` setting for cookies
    According to the documentation this is a deprecated flag however it
    looks like environments are rejecting the `secure` flag even though the
    proxy settings look to be correctly configured in the express app.
    
    The issue is likely caused by a header not being correctly set by the
    upstread Nginx proxy, until that has been looked into, try using this
    flag.
    
    Add an environment variable to allow different environments to reduce
    session time if required.

--- src/config/server.js
+++ src/config/server.js
@@ -4,7 +4,8 @@ const expectedServerEnvironmentValues = {
   PORT: Joi.number().integer().required(),
   COOKIE_SESSION_ENCRYPTION_SECRET: Joi.string().required(),
   HTTP_PROXY: Joi.string(),
-  HTTPS_PROXY: Joi.string()
+  HTTPS_PROXY: Joi.string(),
+  SESSION_COOKIE_DURATION_IN_MILLIS: Joi.number().integer()
 }
 
 const { error, value: validatedServerEnvironmentValues } = Joi.validate(
