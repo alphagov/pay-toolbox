e9e0862dfe2e1c9ee8de96b855d63453322e0c12
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2021-01-20 16:40:53 +0000

    PP-7663: Download csv with Admin user emails

--- src/lib/gatewayAccounts.ts
+++ src/lib/gatewayAccounts.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Service } from './pay-request/types/adminUsers'
+import { parse, ParsedQs, stringify } from 'qs'
+import { BooleanFilterOption } from '../web/modules/common/BooleanFilterOption'
 
 export function aggregateServicesByGatewayAccountId(services: Service[]): { [key: string]: Service } {
   return services
@@ -8,4 +11,37 @@ export function aggregateServicesByGatewayAccountId(services: Service[]): { [key
       })
       return aggregate
     }, {})
+}
+
+
+export function extractFiltersFromQuery(query: ParsedQs): Filters {
+  return {
+    live: query.live as BooleanFilterOption || BooleanFilterOption.True,
+    provider: query.provider as string,
+    three_ds: query.three_ds as BooleanFilterOption || BooleanFilterOption.All,
+    apple_pay: query.apple_pay as BooleanFilterOption || BooleanFilterOption.All,
+    google_pay: query.google_pay as BooleanFilterOption || BooleanFilterOption.All,
+    moto: query.moto as BooleanFilterOption || BooleanFilterOption.All,
+  }
+}
+
+export interface Filters {
+  live: BooleanFilterOption,
+  provider: string,
+  three_ds: BooleanFilterOption,
+  apple_pay: BooleanFilterOption,
+  google_pay: BooleanFilterOption,
+  moto: BooleanFilterOption
+}
+
+
+export function toAccountSearchParams(filters: Filters) {
+  return {
+    type: filters.live === BooleanFilterOption.True && 'live' || filters.live === BooleanFilterOption.False && 'test' || null,
+    payment_provider: filters.provider,
+    requires_3ds: BooleanFilterOption.toNullableBooleanString(filters.three_ds),
+    apple_pay_enabled: BooleanFilterOption.toNullableBooleanString(filters.apple_pay),
+    google_pay_enabled: BooleanFilterOption.toNullableBooleanString(filters.google_pay),
+    moto_enabled: BooleanFilterOption.toNullableBooleanString(filters.moto)
+  }
 }
