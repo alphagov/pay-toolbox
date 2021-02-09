b854f1e36725e044a8de8334b7270a69cafc29da
Aditya Pahuja <aditya.pahuja@digital.cabinet-office.gov.uk>
2021-02-09 14:36:46 +0000

    BAU: Bump class-validator from 0.11.0 to 0.13.1
    This change bumps [class-validator](https://github.com/typestack/class-validator) from 0.11.0 to 0.13.1.
    - [Release notes](https://github.com/typestack/class-validator/releases)
    - [Changelog](https://github.com/typestack/class-validator/blob/develop/CHANGELOG.md)
    - [Commits](typestack/class-validator@v0.11.0...v0.13.1)
    
    It replaces 'ZZ' with undefined value in region parameter in UpdatePhoneNumberFormRequest.ts to fix the following error message 'TS2345: Argument of type '"ZZ"' is not assignable to parameter of type 'CountryCode'. Source: https://github.com/typestack/class-validator/issues/871

--- src/web/modules/users/UpdatePhoneNumberForm.ts
+++ src/web/modules/users/UpdatePhoneNumberForm.ts
@@ -3,7 +3,7 @@ import { IsPhoneNumber, IsNotEmpty, IsString } from 'class-validator'
 import Validated from '../common/validated'
 
 class UpdatePhoneNumberFormRequest extends Validated {
-  @IsPhoneNumber('ZZ', { message: 'User telephone number must be a valid international phone number' })
+  @IsPhoneNumber(undefined, { message: 'User telephone number must be a valid international phone number' })
   @IsNotEmpty()
   @IsString()
   public telephone_number: string;
