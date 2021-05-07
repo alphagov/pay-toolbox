33ae315390f87894200a3dc303b1a00470564239
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2021-05-07 11:32:23 +0100

    PP-8032 Simplify sending errors to sentry
    Simplify by removing the `ManagedError` type, which was used by the
    Sentry error handler middleware to decide whether or not to send an
    error to Sentry.
    
    Instead remove the sentry error handler middleware and instead manually
    emit the errors we want captured in Sentry in our error handler
    middleware.

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -2,21 +2,11 @@
 import { ValidationError as ClassValidatorError } from 'class-validator'
 import { AxiosError } from 'axios'
 
-// any error managed/ handled by the app (will not be thrown to upstream
-// services like Sentry)
-export class ManagedError extends Error {
-  public isManaged: boolean
-  public constructor(message: string, isManaged: boolean = true) {
-    super(message)
-    this.isManaged = isManaged
-  }
-}
-
-export class RequestError extends ManagedError {
+export class RequestError extends Error {
   public name: string
 
-  public constructor(message: string, isManaged?: boolean) {
-    super(message, isManaged)
+  public constructor(message: string) {
+    super(message)
     this.name = this.constructor.name
     Error.captureStackTrace(this, this.constructor)
   }
@@ -25,15 +15,15 @@ export class RequestError extends ManagedError {
 export class EntityNotFoundError extends RequestError {
   public data: { name: string; identifier: string }
 
-  public constructor(name: string, identifier: string, description: string = 'ID', isManaged?: boolean) {
-    super(`${name} with ${description} ${identifier} was not found.`, isManaged)
+  public constructor(name: string, identifier: string, description: string = 'ID') {
+    super(`${name} with ${description} ${identifier} was not found.`)
     this.data = { name, identifier }
   }
 }
 
 // wrap errors from other frameworks in a format that this service can report on
 // @FIXME(sfount) stack trace isn't respected
-export class RESTClientError extends ManagedError {
+export class RESTClientError extends Error {
   public name: string
 
   public data: AxiosError
@@ -50,7 +40,7 @@ export class RESTClientError extends ManagedError {
 }
 
 // expects to be passed a class-validator ValidationError object
-export class IOValidationError extends ManagedError {
+export class IOValidationError extends Error {
   public source: ClassValidatorError[]
 
   public constructor(validations: ClassValidatorError[]) {
@@ -60,7 +50,7 @@ export class IOValidationError extends ManagedError {
   }
 }
 
-export class ValidationError extends ManagedError {
+export class ValidationError extends Error {
   public name: string
 
   public constructor(message: string) {
@@ -70,7 +60,7 @@ export class ValidationError extends ManagedError {
   }
 }
 
-export class NotImplementedError extends ManagedError {
+export class NotImplementedError extends Error {
   public name: string
 
   public constructor(message: string) {
