f621c1335ac93cced54781c13dd6e68b1a587a8f
Iqbal AHmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2023-02-20 15:29:56 +0000

    PP-10734 - Revert Webhook list page changes.
    Revert this changes until we have setup the connectivity between Toolbox & Webhooks.
    
    1. Revert "Merge pull request #1606 from alphagov/pp-10734-webhooks-list-page-fix-pay-request-bug"
    
    This reverts commit 0b5cca9b7d8ba926d7b746a0869b64aa2059f42a, reversing
    changes made to cd37c88a2203187a8fb058edba5164c42aab91fd.
    
    2.  This is the commit message #2:
    
    Revert "Merge pull request #1604 from alphagov/pp-10734-webhooks-list-page"
    
    This reverts commit df7dbdf64638b18c76fdfe36f42ae5359f197789, reversing
    changes made to 0ae53f9f440ae5fd19fa61d2aa0e281e1f9992df.

--- src/lib/pay-request/client.ts
+++ src/lib/pay-request/client.ts
@@ -3,7 +3,6 @@ import _AdminUsers from './services/admin_users/client'
 import _Ledger from './services/ledger/client'
 import _Products from './services/products/client'
 import _PublicAuth from './services/public_auth/client'
-import _Webhooks from './services/webhooks/client'
 import { PayHooks } from './base'
 import {BroadcastResult} from "./shared";
 
@@ -13,14 +12,12 @@ const clients: {
   Products: _Products;
   PublicAuth: _PublicAuth;
   AdminUsers: _AdminUsers;
-  Webhooks: _Webhooks;
 } = {
   Connector: new _Connector(),
   Ledger: new _Ledger(),
   Products: new _Products(),
   PublicAuth: new _PublicAuth(),
-  AdminUsers: new _AdminUsers(),
-  Webhooks: new _Webhooks()
+  AdminUsers: new _AdminUsers()
 }
 
 export const Connector = clients.Connector
@@ -28,7 +25,6 @@ export const Ledger = clients.Ledger
 export const Products = clients.Products
 export const PublicAuth = clients.PublicAuth
 export const AdminUsers = clients.AdminUsers
-export const Webhooks = clients.Webhooks
 
 export function config(env: NodeJS.ProcessEnv = {}, options: PayHooks = {}): void {
   const connectorUrl = env.CONNECTOR_URL
@@ -36,7 +32,6 @@ export function config(env: NodeJS.ProcessEnv = {}, options: PayHooks = {}): voi
   const ledgerUrl = env.LEDGER_URL
   const productsUrl = env.PRODUCTS_URL
   const publicAuthUrl = env.PUBLIC_AUTH_URL
-  const webhooksUrl = env.WEBHOOKS_URL
 
   if (connectorUrl)
     clients.Connector._configure(connectorUrl, options)
@@ -48,8 +43,6 @@ export function config(env: NodeJS.ProcessEnv = {}, options: PayHooks = {}): voi
     clients.Products._configure(productsUrl, options)
   if (publicAuthUrl)
     clients.PublicAuth._configure(publicAuthUrl, options)
-  if (webhooksUrl)
-    clients.Webhooks._configure(webhooksUrl, options)
 }
 
 // make a GET request to all supported clients - for now suppress throwing the
