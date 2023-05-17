ac03cb3bffc8b36e2959106881842ac2f81f1adc
Krishna Bottla <40598480+kbottla@users.noreply.github.com>
2023-05-17 11:44:51 +0100

    BAU - Handle inactive agreement status
    - Display correct tag for inactive agreement status
    - Also removed `expired` status which is no longer needed

--- src/web/modules/agreements/states.ts
+++ src/web/modules/agreements/states.ts
@@ -1,14 +1,15 @@
 import {ExternalAgreementState, ExternalTransactionState} from "../../../lib/pay-request/shared";
 
 export enum AgreementListFilterStatus {
-  Created = 'created', Active = 'active', Cancelled = 'cancelled', All = 'all'
+  Active = 'active', All = 'all', Cancelled = 'cancelled', Created = 'created', Inactive = 'inactive'
 }
 
 const agreementFilterStatusMap: { [key in AgreementListFilterStatus]: ExternalAgreementState } = {
   all: null,
   created: ExternalAgreementState.Created,
   active: ExternalAgreementState.Active,
-  cancelled: ExternalAgreementState.Cancelled
+  cancelled: ExternalAgreementState.Cancelled,
+  inactive: ExternalAgreementState.Inactive
 }
 
 export function resolveAgreementStates(statusFilter: AgreementListFilterStatus) {
