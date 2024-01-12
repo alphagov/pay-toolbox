ccbaad4b73dc0fb8f70a31473062ca5d30d8544a
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-01-12 17:35:56 +0000

    PP-11212 Use correct live/test Stripe API key
    Make it possible to retreive/update details for Stripe test account by
    using the correct API key depending on the account type.
    
    Move the Stripe API configuration to a single place in
    stripe.client.ts. Had to use a `// @ts-ignore` in account.ts
    to get this working with Typescript.

---
+++ src/lib/stripe/stripe.client.ts
@@ -0,0 +1,22 @@
+import HTTPSProxyAgent from 'https-proxy-agent'
+import Stripe from "stripe";
+import * as config from '../../config'
+
+const STRIPE_ACCOUNT_API_KEY = process.env.STRIPE_ACCOUNT_API_KEY || ''
+const STRIPE_ACCOUNT_TEST_API_KEY = process.env.STRIPE_ACCOUNT_TEST_API_KEY || ''
+
+const stripeConfig: Stripe.StripeConfig = {
+  'apiVersion': '2020-08-27'
+}
+
+if (config.server.HTTPS_PROXY) {
+  // @ts-ignore
+  stripeConfig.httpAgent = new HTTPSProxyAgent(config.server.HTTPS_PROXY)
+}
+
+const stripeApi = new Stripe(STRIPE_ACCOUNT_API_KEY, stripeConfig)
+const stripeTestApi = new Stripe(STRIPE_ACCOUNT_TEST_API_KEY, stripeConfig)
+
+export function getStripeApi(live: boolean): Stripe {
+  return live && stripeApi || stripeTestApi
+}
