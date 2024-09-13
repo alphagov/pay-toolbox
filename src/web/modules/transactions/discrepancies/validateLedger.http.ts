fbda7c31921be2f2c465c6fde6094a4fd95ca370
Natalie Carey <natalie.carey@digital.cabinet-office.gov.uk>
2024-09-13 15:11:15 +0100

    Inital simplification of parity summary page.

--- src/web/modules/transactions/discrepancies/validateLedger.http.ts
+++ src/web/modules/transactions/discrepancies/validateLedger.http.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
-import { diff } from 'deep-diff'
+import {diff, DiffEdit} from 'deep-diff'
 import { Ledger, Connector } from '../../../../lib/pay-request/client'
+import * as _ from 'lodash'
+import moment from 'moment'
+
+const EDIT = 'E'
 
 export async function validateLedgerTransaction(
   req: Request,
@@ -10,7 +14,35 @@ export async function validateLedgerTransaction(
   try {
     const ledgerEntry = await Ledger.transactions.retrieve(req.params.id)
     const connectorEntry = await Connector.charges.retrieveAPI(req.params.id, ledgerEntry.gateway_account_id)
-    const parity = diff(connectorEntry, ledgerEntry)
+
+    const ledgerPreparedForDiff = _.omit(ledgerEntry, [
+      'gateway_account_id',
+      'transaction_id',
+      'disputed',
+      'source',
+      'live',
+      'transaction_type',
+      'credential_external_id',
+      'service_id',
+      'refund_summary.amount_refunded'
+    ])
+    const connectorPreparedForDiff = _.omit(connectorEntry, [
+        'links',
+        'charge_id'
+    ])
+
+    const parity = diff(connectorPreparedForDiff, ledgerPreparedForDiff).filter(obj => {
+        if (obj.path[0] !== 'created_date' && obj.kind === EDIT) {
+            return true
+        }
+        const castedObj = obj as DiffEdit<string>
+        const lhsDate = moment(castedObj.lhs)
+        const rhsDate = moment(castedObj.rhs)
+        if (Math.abs(lhsDate.diff(rhsDate)) > 10) {
+            return true
+        }
+        return false
+    })
 
     res.render('transactions/discrepancies/validateLedger', { ledgerEntry, connectorEntry, parity })
   } catch (error) {
