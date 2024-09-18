d899a4f8b031a799609156a11351398ec9c34600
James Peacock <james.peacock@digital.cabinet-office.gov.uk>
2024-09-18 17:37:27 +0100

    PP-12854 Distinguish between diff kinds and improve layout
    - correct the diff filtering logic so that relevant differences are all included
    - lay out page so that the diff kinds (different fields, missing fields) are under sub-headings

--- src/web/modules/transactions/discrepancies/validateLedger.http.ts
+++ src/web/modules/transactions/discrepancies/validateLedger.http.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction } from 'express'
-import {diff, DiffEdit} from 'deep-diff'
+import {diff, DiffDeleted, DiffEdit, DiffNew} from 'deep-diff'
 import { Ledger, Connector } from '../../../../lib/pay-request/client'
 import * as _ from 'lodash'
 import moment from 'moment'
 
 const EDIT = 'E'
+const DELETED = 'D'
+const NEW = 'N'
 
 export async function validateLedgerTransaction(
   req: Request,
@@ -14,9 +16,9 @@ export async function validateLedgerTransaction(
   try {
     const ledgerEntry = await Ledger.transactions.retrieve(req.params.id)
     const connectorEntry = await Connector.charges.parityCheck(req.params.id, ledgerEntry.gateway_account_id)
-    let message
+    let warningMessage
     if (typeof connectorEntry === 'string') {
-        message = connectorEntry
+        warningMessage = connectorEntry
     }
 
     const ledgerResponseWithoutLedgerSpecificFields = _.omit(ledgerEntry, [
@@ -47,18 +49,43 @@ export async function validateLedgerTransaction(
     ])
 
     const parity = diff(connectorResponseWithoutConnectorSpecificFields, ledgerResponseWithoutLedgerSpecificFields).filter(obj => {
-        if (obj.path[0] !== 'created_date' && obj.kind === EDIT) {
-            return true
+        switch (obj.kind) {
+            case EDIT: {
+                if (obj.path[0] !== 'created_date') {
+                    return true
+                } else {
+                    const castedDiffObj = obj as DiffEdit<string>
+                    const lhsDate = moment(castedDiffObj.lhs)
+                    const rhsDate = moment(castedDiffObj.rhs)
+                    return Math.abs(lhsDate.diff(rhsDate)) > 10;
+                }
+            }
+            case DELETED: {
+                const castedDeletedObj = obj as DiffDeleted<string>
+                return castedDeletedObj.lhs != null;
+            }
+            case NEW: {
+                const castedNewObj = obj as DiffNew<string>
+                return castedNewObj.rhs != null;
+            }
         }
-        const castedObj = obj as DiffEdit<string>
-        const lhsDate = moment(castedObj.lhs)
-        const rhsDate = moment(castedObj.rhs)
-        if (Math.abs(lhsDate.diff(rhsDate)) > 10) {
-            return true
-        }
-        return false
     })
-    res.render('transactions/discrepancies/validateLedger', { ledgerEntry, connectorEntry, parity, message})
+    let parityDisplay
+    if (parity.length > 0) {
+        const diffEdit = parity.filter((obj) => obj.kind === EDIT)
+        const diffNew = parity.filter((obj) => obj.kind === NEW)
+        const diffDelete = parity.filter((obj) => obj.kind === DELETED)
+        if (diffEdit.length > 0) {
+            parityDisplay = { 'Fields which have different values': diffEdit }
+        }
+        if (diffNew.length > 0) {
+            parityDisplay = { ...parityDisplay, 'Fields missing from Connector': diffNew }
+        }
+        if (diffDelete.length > 0) {
+            parityDisplay = { ...parityDisplay, 'Fields missing from Ledger': diffDelete }
+        }
+    }
+    res.render('transactions/discrepancies/validateLedger', { ledgerEntry, connectorEntry, parityDisplay, warningMessage})
   } catch (error) {
     next(error)
   }
