fce8b7db0d33fb02d1fdce2e4fb68e6b9dddd7ab
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2025-01-16 15:26:43 +0000

    PP-13510: Remove pence from currency formatter

--- browser/src/dashboard/format.ts
+++ browser/src/dashboard/format.ts
@@ -1,6 +1,8 @@
 export const currencyFormatter = new Intl.NumberFormat('en-GB', {
   style: 'currency',
-  currency: 'GBP'
+  currency: 'GBP',
+  maximumFractionDigits: 0,
+  minimumFractionDigits: 0
 })
 
 export const numberFormatter = new Intl.NumberFormat('en-GB', {
