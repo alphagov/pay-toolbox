f51bd9c2097c6e9beb112e22638aa41d9f08c7be
Hugo Jeffreys <hugo.jeffreys@digital.cabinet-office.gov.uk>
2026-04-08 17:10:12 +0100

    PP-14951 upgrade to passport v7

--- src/lib/auth/github/strategy.js
+++ src/lib/auth/github/strategy.js
@@ -1,5 +1,5 @@
 // github OAuth strategy
-const { Strategy } = require('passport-github')
+const { Strategy } = require('passport-github2')
 const config = require('../../../config')
 const logger = require('../../logger')
 
