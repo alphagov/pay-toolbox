e769b6fa928af0f81219cf50acdc417757fdbc6e
christiandutton-mt <christian.dutton@madetech.com>
2026-08-04 16:16:38 +0100

    PP-15673 add heading and caption

--- src/web/modules/services/features/features.http.ts
+++ src/web/modules/services/features/features.http.ts
@@ -1,5 +1,20 @@
-import { Request, Response } from 'express'
+import {NextFunction, Request, Response} from 'express'
+import {AdminUsers} from "../../../../lib/pay-request/client";
 
-export async function get(req: Request, res: Response): Promise<void> {
-  res.render('services/features/update')
+
+export async function get(req: Request, res: Response, next: NextFunction): Promise<void> {
+
+    try {
+        const featureName = req.params.feature
+
+        const service = await AdminUsers.services.retrieve(req.params.id)
+        const {name : serviceName, external_id: id} = service
+
+        res.render('services/features/update', { serviceName, id, featureName })
+    }
+    catch (error){
+        next(error)
+    }
+
+
 }
