8f29a62c79a139f9f094b20795377d88d1015b38
Ralph Mallett <ralph.mallett@digital.cabinet-office.gov.uk>
2026-08-05 13:54:21 +0100

    PP-15673 add radio button to update feature view

--- src/web/modules/services/features/features.http.ts
+++ src/web/modules/services/features/features.http.ts
@@ -1,20 +1,20 @@
-import {NextFunction, Request, Response} from 'express'
-import {AdminUsers} from "../../../../lib/pay-request/client";
+import { NextFunction, Request, Response } from 'express'
+import { AdminUsers } from "../../../../lib/pay-request/client";
 
 
 export async function get(req: Request, res: Response, next: NextFunction): Promise<void> {
 
-    try {
-        const featureName = req.params.feature
+  try {
+    const featureName = req.params.feature
+    const service = await AdminUsers.services.retrieve(req.params.id)
+    const { name: serviceName, external_id: id } = service
+    const enabled = service.service_features[featureName].enabled
 
-        const service = await AdminUsers.services.retrieve(req.params.id)
-        const {name : serviceName, external_id: id} = service
+    console.log("ebaled: " + enabled)
 
-        res.render('services/features/update', { serviceName, id, featureName })
-    }
-    catch (error){
-        next(error)
-    }
-
-
+    res.render('services/features/update', { serviceName, id, featureName, enabled })
+  }
+  catch (error) {
+    next(error)
+  }
 }
